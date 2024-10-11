88ba24c8d4c984fc955c1be29aba69395dbc06ed
KANA <gomadango616@gmail.com>
2024-10-11 13:23:09 +0900

    fix: マイグレーションファイルを正しく適用

--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,8 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_10_125753) do
     t.string "email", null: false
     t.string "crypted_password"
     t.string "salt"
+    t.string "first_name", null: false
+    t.string "last_name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
