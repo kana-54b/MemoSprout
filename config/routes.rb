3a8b67933c4ecc98cc59b08f41ed00edc6c077f9
KANA <gomadango616@gmail.com>
2024-10-11 13:27:13 +0900

    add: ユーザー登録機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root "top#index"
   get "top/index"
 
+  resources :users, only: %i[new create]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
