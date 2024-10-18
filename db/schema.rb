408ed5805561a97f3fa9f6d8a02c358533a0fd56
KANA <gomadango616@gmail.com>
2024-10-18 13:16:07 +0900

    add: memosテーブル マイグレーション実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_10_125753) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_18_033919) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "memos", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.text "emotion", null: false
+    t.date "date"
+    t.text "memo_content", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_memos_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
@@ -24,4 +34,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_10_125753) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
+
+  add_foreign_key "memos", "users"
 end
