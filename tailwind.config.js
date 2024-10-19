271dc50b6ed448fa01de2719a5b1a1d619e2f309
KANA <gomadango616@gmail.com>
2024-10-19 22:24:24 +0900

    add: safelistにフラッシュメッセージのクラス

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
+// tailwind.config.js
+
 module.exports = {
   content: [
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
+  ],
+  safelist: [
+    'alert', 
+    'alert-success', 
+    'alert-error', 
+    'alert-info', 
+    'alert-warning'
   ],
   plugins: [require("daisyui")],
   daisyui: {
