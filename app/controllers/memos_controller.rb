9a1bbff1bdda5822611b204e5920f21e4c6f75a6
KANA <gomadango616@gmail.com>
2024-10-24 12:04:06 +0900

    add: confirmアクション

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -5,8 +5,24 @@ class MemosController < ApplicationController
     @memo = Memo.new
   end
 
+  def confirm
+    unless params[:memo].present? # memos/confirmに直接アクセスした場合の対応
+      redirect_to new_memo_path, error: "フォームから入力してください"
+      return
+    end
+
+    @memo = current_user.memos.build(memo_params)
+    if @memo.invalid? # バリデーションエラーがある場合
+      flash.now[:error] = "メモの作成に失敗しました😨"
+      render :new, status: :unprocessable_entity
+      return
+    end
+      Rails.logger.debug "confirmアクション Memo_params内容🎃🎃🎃: #{memo_params.inspect}" # デバッグ用ログ
+      render :confirm
+  end
+
   def create
-    Rails.logger.debug "Memo_params内容🌱🌱🌱: #{memo_params.inspect}" # デバッグ用ログ
+    Rails.logger.debug "createアクション Memo_params内容🌱🌱🌱: #{memo_params.inspect}" # デバッグ用ログ
     @memo = current_user.memos.build(memo_params)
     if @memo.save
       redirect_to new_memo_path, success: "メモを作成しました"
