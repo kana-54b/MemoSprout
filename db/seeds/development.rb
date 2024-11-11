afb838b4c08cbe780afa21261bc4904d2568f8b6
KANA <gomadango616@gmail.com>
2024-11-11 15:51:32 +0900

    add: gemでダミーデータを追加

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -1,26 +1,41 @@
-User.create!([
-  { email: "b@b", password: "pass", password_confirmation: "pass", first_name: "やまだ", last_name: "はなこ" },
-  { email: "c@c", password: "pass", password_confirmation: "pass", first_name: "たけだ", last_name: "じろう" },
-  { email: "d@d", password: "pass", password_confirmation: "pass", first_name: "さとう", last_name: "たろう" }
-])
+# User.create!([
+#   { email: "b@b", password: "pass", password_confirmation: "pass", first_name: "やまだ", last_name: "はなこ" },
+#   { email: "c@c", password: "pass", password_confirmation: "pass", first_name: "たけだ", last_name: "じろう" },
+#   { email: "d@d", password: "pass", password_confirmation: "pass", first_name: "さとう", last_name: "たろう" }
+# ])
 
-Memo.create!([
-  { emotion: "happy",
-  memo_content: { "what" => "いいことの内容", "why" => "なぜの内容", "why_more" => "なぜの掘り下げ", "how" => "どうやって？", "summary" => "まとめ〜" },
-  user_id: 1 },
-  { emotion: "angry",
-  memo_content: { "what" => "悪いことの内容", "why" => "なぜの内容", "why_more" => "なぜの掘り下げ", "how" => "どうやって？", "summary" => "まとめ〜" },
-  user_id: 1 },
-  { emotion: "happy",
-  memo_content: { "what" => "いいことの内容", "why" => "なぜの内容", "why_more" => "なぜの掘り下げ", "how" => "どうやって？", "summary" => "まとめ〜" },
-  user_id: 2 },
-  { emotion: "angry",
-  memo_content: { "what" => "悪いことの内容", "why" => "なぜの内容", "why_more" => "なぜの掘り下げ", "how" => "どうやって？", "summary" => "まとめ〜" },
-  user_id: 2 },
-  { emotion: "happy",
-  memo_content: { "what" => "いいことの内容", "why" => "なぜの内容", "why_more" => "なぜの掘り下げ", "how" => "どうやって？", "summary" => "まとめ〜" },
-  user_id: 3 },
-  { emotion: "angry",
-  memo_content: { "what" => "悪いことの内容", "why" => "なぜの内容", "why_more" => "なぜの掘り下げ", "how" => "どうやって？", "summary" => "まとめ〜" },
-  user_id: 3 }
-])
+# Memo.create!([
+#   { emotion: "happy",
+#   memo_content: { "what" => "いいことの内容", "why" => "なぜの内容", "why_more" => "なぜの掘り下げ", "how" => "どうやって？", "summary" => "まとめ〜" },
+#   user_id: 1 },
+#   { emotion: "angry",
+#   memo_content: { "what" => "悪いことの内容", "why" => "なぜの内容", "why_more" => "なぜの掘り下げ", "how" => "どうやって？", "summary" => "まとめ〜" },
+#   user_id: 1 },
+#   { emotion: "happy",
+#   memo_content: { "what" => "いいことの内容", "why" => "なぜの内容", "why_more" => "なぜの掘り下げ", "how" => "どうやって？", "summary" => "まとめ〜" },
+#   user_id: 2 },
+#   { emotion: "angry",
+#   memo_content: { "what" => "悪いことの内容", "why" => "なぜの内容", "why_more" => "なぜの掘り下げ", "how" => "どうやって？", "summary" => "まとめ〜" },
+#   user_id: 2 },
+#   { emotion: "happy",
+#   memo_content: { "what" => "いいことの内容", "why" => "なぜの内容", "why_more" => "なぜの掘り下げ", "how" => "どうやって？", "summary" => "まとめ〜" },
+#   user_id: 3 },
+#   { emotion: "angry",
+#   memo_content: { "what" => "悪いことの内容", "why" => "なぜの内容", "why_more" => "なぜの掘り下げ", "how" => "どうやって？", "summary" => "まとめ〜" },
+#   user_id: 3 }
+# ])
+
+
+# faker data
+10.times do |n|
+  Memo.create!(
+    user_id: 1,
+    memo_content: {
+      "what" => "テスト#{ n + 1 }",
+      "why" => "なぜの内容",
+      "why_more" => "なぜの掘り下げ",
+      "how" => "どうやって？",
+      "summary" => "#{ Faker::JapaneseMedia::SwordArtOnline.real_name } - #{ Faker::JapaneseMedia::SwordArtOnline.game_name }"
+    }
+  )
+end
