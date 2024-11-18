0d73e56f12ba00d189af6414779885a2ebb0e6d8
KANA <gomadango616@gmail.com>
2024-11-18 12:47:15 +0900

    add: 連続記録日数を表示

--- app/models/memo.rb
+++ app/models/memo.rb
@@ -25,4 +25,32 @@ class Memo < ApplicationRecord
   def favorited_by?(user) # current_userがお気に入り登録しているかを確認
     memo_favorite&.user_id == user.id
   end
+
+  def self.streak_days(user) # 連続記録
+    Rails.logger.debug("DEBUG: streak_daysがよばれました。このユーザーに: #{user.id}")
+
+    # メモの作成日を取得し日時を日付に変換、日付の重複を削除。
+    dates = user.memos.order(created_at: :asc).pluck(:created_at).map(&:to_date).uniq
+    Rails.logger.debug("dates📅: #{dates} - USER👦: #{user.id}")
+
+    streak_count = 1 # 連続記録をカウントする
+    previous_date = nil # 直前の日付を保持する
+
+    dates.each do |date|
+      current_date = date
+      Rails.logger.debug("current_date🍎: #{current_date}, previous_date🍏: #{previous_date}, streak_count#️⃣: #{streak_count}")
+
+    if previous_date.nil? || current_date == previous_date + 1 # 直前の日付がnilの場合、または今日の日付が直前の日付+1の場合
+      streak_count += 1 unless previous_date.nil? # 直前の日付がnilではない時（直前の日付が入っている時）に、streak_countを+1する
+    elsif current_date > previous_date + 1 # 今日の日付が直前の日付+1よりも大きい場合(1日より空いている場合)
+      Rails.logger.debug "🐛:日付が空いたため中断" 
+      break # ループを抜ける
+    end
+
+      previous_date = current_date # 直前の日付 ＝　今日の日付に更新
+    end
+
+    Rails.logger.debug "DEBUG🔥: 最終的なstreak_count: #{streak_count}"
+    streak_count # 計算結果を返す
+  end
 end
