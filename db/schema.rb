d8ecf8d46ccf4b47c81060e8792ca8a2220bc69d
KANA <gomadango616@gmail.com>
2024-12-09 22:52:50 +0900

    change: 姓名カラムのnullを許可する

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_04_135540) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_09_063303) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -46,8 +46,8 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_04_135540) do
     t.string "email", null: false
     t.string "crypted_password"
     t.string "salt"
-    t.string "first_name", null: false
-    t.string "last_name", null: false
+    t.string "first_name"
+    t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
