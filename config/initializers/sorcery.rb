e58aeb5f80cf4d4de6dbcd0cf7f6518fe94d41a0
KANA <gomadango616@gmail.com>
2024-12-10 15:18:01 +0900

    fix: secretのスペルミスを修正

--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -159,7 +159,7 @@ Rails.application.config.sorcery.configure do |config|
   # config.auth0.site = "https://example.auth0.com"
   #
   config.google.key = Rails.application.credentials.dig(:google, :client_id)
-  config.google.secret = Rails.application.credentials.dig(:google, :client_seacret)
+  config.google.secret = Rails.application.credentials.dig(:google, :client_secret)
   config.google.callback_url = "http://localhost:3000/oauth/callback?provider=google"
   config.google.user_info_mapping = { email: "email", first_name: "given_name", last_name: "family_name" }
   # config.google.scope = "https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile"
