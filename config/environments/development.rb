9356d2cb410201969c89e5c9f2ae724e5fb4283e
KANA <gomadango616@gmail.com>
2025-01-29 23:04:57 +0900

    fix: rubocopテストのLintエラー部分を修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
- 
+
   # The email will not be sent but will just pop up in your browser.
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
