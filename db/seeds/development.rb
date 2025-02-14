0b407449449da07ecfc3e94596da2c750960d3c6
KANA <gomadango616@gmail.com>
2025-02-14 16:10:11 +0900

    seed: シードデータ編集

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -5,18 +5,20 @@
 #   { email: "d@d", password: "pass", password_confirmation: "pass", first_name: "さとう", last_name: "たろう" }
 # ])
 
+emotions = [ "happy", "angry", "sad", "funny", "other" ]
+
 # faker data
 10.times do |n|
   Memo.create!(
-    user_id: 1,
-    date: "2024-01-01",
-    emotion: "funny",
+    user_id: 2,
+    date: "2025-01-01",
+    emotion: emotions.sample,
     memo_content: {
       "what" => "テスト#{ n + 1 }",
       "why" => "なぜの内容",
       "why_more" => "なぜの掘り下げ",
       "how" => "どうやって？",
-      "summary" => "#{ Faker::Lorem.paragraph(sentence_count: 2) } (#{ Faker::Lorem.sentence(word_count: 1) })"
+      "summary" => "#{ Faker::Lorem.paragraph(sentence_count: 1) } (#{ Faker::Lorem.sentence(word_count: 1) })"
     }
   )
 end
